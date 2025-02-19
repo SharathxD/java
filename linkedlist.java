75fa2e1399d73324633eef9b4776c17fe2ff7ecf
Sharath Gowda <msharathgowda7@gmail.com>
2025-02-19 19:03:41 +0530

    LinkedList Example

---
+++ linkedlist.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class linkedlist {
+    public static void main(String[] args) {
+        LinkedList<Integer> ll=new LinkedList<>();
+        //addFirst()
+        ll.addFirst(20);
+        ll.add(30);
+        ll.addFirst(10);
+        System.out.println(ll);
+        //addLast()
+        ll.addLast(100);
+        ll.addLast(200);
+        System.out.println("LL :" +ll);
+        LinkedList<Integer> lll=new LinkedList<>();
+        //addAll()
+        lll.addAll(ll);
+        System.out.println("LLL : "+lll);
+        //getFirst() & getLast() for getting elements
+        System.out.println("First ele in LL "+ll.getFirst());
+        System.out.println("Last ele in LLL "+lll.getLast());
+        System.out.println("Class of this DataType is : "+ll.getClass());
+    }
+}
