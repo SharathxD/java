ee2761c33a77027809e5b854f43e501ea0919426
Sharath Gowda <msharathgowda7@gmail.com>
2025-02-19 18:43:23 +0530

    ArrayList

--- arraylist.java
+++ arraylist.java
@@ -11,5 +11,28 @@ public class arraylist {
         // can add null to arraylist
         al.add(null);
         System.out.println(al);
-    }
+        // add at specific index
+        al.add(0, null);
+        System.out.println(al);
+        //removing the elements it uses index value
+        al.remove(1);
+        System.out.println(al);
+        //removing all from the list
+        al.removeAll(al);
+        System.out.println(al);
+        //length
+        System.out.println("Size : "+al.size());
+
+        al.add(10);
+        al.add(10);
+        al.add(10);
+        al.add(10);
+        al.add(10);
+        al.add(10);
+        //accessing using for each loop
+        for (Integer it : al) {
+            System.out.println(it);
+            
+        }
+}
 }
