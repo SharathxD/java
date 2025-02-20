acaa3bc83b043dedfd764da0bb7e23c8ec0bfc66
Sharath Gowda <msharathgowda7@gmail.com>
2025-02-20 19:51:54 +0530

    Collections

---
+++ Set Interface/hashset.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class hashset{
+    public static void main(String[] args) {
+        HashSet<Integer> hs=new HashSet<>();
+        hs.add(10);
+        hs.size();
+        hs.isEmpty();
+        hs.clear();
+        hs.isEmpty();
+    }
+}
