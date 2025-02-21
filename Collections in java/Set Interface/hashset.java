ba00fe715b065bd09f51129780eba6ef2ffc31ae
Sharath Gowda <msharathgowda7@gmail.com>
2025-02-21 19:24:58 +0530

    hashset

--- Collections in java/Set Interface/hashset.java
+++ Collections in java/Set Interface/hashset.java
@@ -4,10 +4,21 @@ class hashset{
         HashSet<Integer> hs=new HashSet<>();
         // add element
         hs.add(10);
+        hs.add(20);
+        System.out.println(hs);
         // size of the set
-        hs.size();
-        hs.isEmpty();
+        System.out.println(hs.size());
+        // isEmpty()
+        System.out.println(hs.isEmpty());
+        //clear() to clear the set
         hs.clear();
-        hs.isEmpty();
+        System.out.println(hs);
+        
+        hs.add(10);
+        hs.add(20);
+        hs.add(30);
+        hs.add(40);
+        // contains returns true or false
+        System.out.println(hs.contains(120));
     }
 }
